27aed40c8eb33ca2ed27653ad6ec1a16c31a486f
Márcio Duarte <zmduartez@gmail.com>
2020-05-08 12:16:08 +0100

    Update billboard
        1. Add billboard legs
        2. Add gradient texture

--- src/billboard/MyBillboard.js
+++ src/billboard/MyBillboard.js
@@ -10,6 +10,9 @@ class MyBillboard extends CGFobject {
         this.counter = new MyPlane(this.scene, 10);
 
         this.whiteTex = new CGFtexture(this.scene, "../resources/billboard/white.png");
+        this.greyTex = new CGFtexture(this.scene, "../resources/billboard/grey.png");
+        this.gradient = new CGFtexture(this.scene)
+
         this.material = new CGFappearance(this.scene);
         this.material.setDiffuse(1, 1, 1, 1);
         this.material.setSpecular(1, 1, 1, 1);
@@ -18,6 +21,10 @@ class MyBillboard extends CGFobject {
 
 	display() {
         this.scene.pushMatrix();
+        
+        // move to the right place
+        this.scene.translate(0, 7.5, 18);
+        this.scene.rotate(2*Math.PI/5, 0, 1, 0);
 
         // draw canvas
         this.material.setTexture(this.whiteTex);
@@ -25,6 +32,23 @@ class MyBillboard extends CGFobject {
         this.scene.pushMatrix();
         this.scene.translate(0, 1.5, 0);
         this.scene.scale(2, 1, 0.1);
+        this.canvas.display();
+        this.scene.popMatrix();
+
+        // draw legs
+        // left
+        this.material.setTexture(this.greyTex);
+        this.material.apply();
+        this.scene.pushMatrix();
+        this.scene.translate(0.9, 0.5, 0);
+        this.scene.scale(0.05, 1, 0.05);
+        this.canvas.display();
+        this.scene.popMatrix();
+
+        // right
+        this.scene.pushMatrix();
+        this.scene.translate(-0.9, 0.5, 0);
+        this.scene.scale(0.05, 1, 0.05);
         this.canvas.display();
         this.scene.popMatrix();
 
